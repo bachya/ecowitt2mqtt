1d90d67069d176eae0dd429847724ddcc4fcf13b
Aaron Bach <bachya1208@gmail.com>
2020-09-24 21:32:29 -0600

    Add initial library functionality (#1)
    * Add various tooling
    
    * Initial working application
    
    * Use info-level supervisord logs
    
    * Add initial library functionality
    
    * Updates
    
    * Update CI
    
    * Updates
    
    * Update CI
    
    * Update README
    
    * Update README
    
    * Update .gitignore

---
+++ .github/pull_request_template.md
@@ -0,0 +1,12 @@
+**Describe what the PR does:**
+
+**Does this fix a specific issue?**
+
+Fixes https://github.com/bachya/ecowitt2mqtt/issues/<ISSUE ID>
+  
+**Checklist:**
+
+- [ ] Confirm that one or more new tests are written for the new functionality.
+- [ ] Run tests and ensure everything passes (with 100% test coverage).
+- [ ] Update `README.md` with any new documentation.
+- [ ] Add yourself to `AUTHORS.md`.
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+if [ -z "${MQTT_BROKER}" ]; then
+    echo "Missing required environment variable: MQTT_BROKER"
+    exit 1
+fi
+
+if [ -z "${MQTT_TOPIC}" ]; then
+    echo "Missing required environment variable: MQTT_TOPIC"
+    exit 1
+fi
+
+ECOWITT2MQTT_ARGS="--mqtt-broker=${MQTT_BROKER} --mqtt-topic=${MQTT_TOPIC}"
+
+if [ -n "${MQTT_PORT}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-port=${MQTT_PORT}"
+fi
+
+if [ -n "${MQTT_USERNAME}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-username=${MQTT_USERNAME}"
+fi
+
+if [ -n "${MQTT_PASSWORD}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-password=${MQTT_PASSWORD}"
+fi
+
+if [ -n "${ENDPOINT}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --endpoint=${ENDPOINT}"
+fi
+
+if [ -n "${PORT}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --port=${PORT}"
+fi
+
+if [ -n "${LOG_LEVEL}" ]; then
+    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --log-level=${LOG_LEVEL}"
+fi
+
+# Store the options string as an environment variable that supervisor can use:
+export ECOWITT2MQTT_ARGS
+
+/usr/bin/supervisord -c /etc/supervisord.conf
