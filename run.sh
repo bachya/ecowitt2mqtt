8775bb4dbfa5e92f43ebbbae68e893b926688588
Aaron Bach <bachya1208@gmail.com>
2020-12-05 11:34:04 -0700

    Remove supervisors from Docker image (#27)
    * Remove use of supervisord in Docker image
    
    * Update shield
    
    * CI

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ -z "${MQTT_BROKER}" ]; then
     echo "Missing required environment variable: MQTT_BROKER"
     exit 1
@@ -9,41 +9,38 @@ if [ -z "${MQTT_TOPIC}" ] && [ "${HASS_DISCOVERY}" != "true" ]; then
     exit 1
 fi
 
-ECOWITT2MQTT_ARGS="--mqtt-broker=${MQTT_BROKER} --mqtt-topic=${MQTT_TOPIC}"
+PARAMS=(--mqtt-broker="${MQTT_BROKER}" --mqtt-topic="${MQTT_TOPIC}")
 
 if [ -n "${LOG_LEVEL}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --log-level=${LOG_LEVEL}"
+    PARAMS+=(--log-level="${LOG_LEVEL}")
 fi
 
 if [ -n "${MQTT_PORT}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-port=${MQTT_PORT}"
+    PARAMS+=(--mqtt-port="${MQTT_PORT}")
 fi
 
 if [ -n "${MQTT_USERNAME}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-username=${MQTT_USERNAME}"
+    PARAMS+=(--mqtt-username="${MQTT_USERNAME}")
 fi
 
 if [ -n "${MQTT_PASSWORD}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --mqtt-password=${MQTT_PASSWORD}"
+    PARAMS+=(--mqtt-password="${MQTT_PASSWORD}")
 fi
 
 if [ "${HASS_DISCOVERY}" = "true" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --hass-discovery"
+    PARAMS+=("--hass-discovery")
 fi
 
 if [ -n "${HASS_DISCOVERY_PREFIX}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --hass-discovery-prefix=${HASS_DISCOVERY_PREFIX}"
+    PARAMS+=(--hass-discovery-prefix="${HASS_DISCOVERY_PREFIX}")
 fi
 
 if [ -n "${ENDPOINT}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --endpoint=${ENDPOINT}"
+    PARAMS+=(--endpoint="${ENDPOINT}")
 fi
 
 if [ -n "${PORT}" ]; then
-    ECOWITT2MQTT_ARGS="${ECOWITT2MQTT_ARGS} --port=${PORT}"
+    PARAMS+=(--port="${PORT}")
 fi
 
-# Store the options string as an environment variable that supervisor can use:
-export ECOWITT2MQTT_ARGS
-
-/usr/bin/supervisord -c /etc/supervisord.conf
+python3 /usr/src/ecowitt2mqtt/main.py "${PARAMS[@]}"
