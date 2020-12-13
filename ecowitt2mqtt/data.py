ba8aa71906013e8c8becc007bbbcef79f4d90868
Aaron Bach <bachya1208@gmail.com>
2020-12-13 13:19:23 -0700

    Fix another type conversion issue with pressure data (#35)

--- ecowitt2mqtt/data.py
+++ ecowitt2mqtt/data.py
@@ -209,7 +209,7 @@ class DataProcessor:  # pylint: disable=too-many-instance-attributes
                     inhg_to_hpa(int(float(self._data[original_key]))), 1
                 )
             else:
-                data[new_key] = int(self._data[original_key])
+                data[new_key] = int(float(self._data[original_key]))
 
         return data
 
