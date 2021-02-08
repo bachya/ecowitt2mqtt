539c577405a257cbeec7b75db3ce59fedfb95b4b
jduv <jduv@7factor.io>
2021-02-07 23:45:52 -0500

    Removing truncating from barometric pressure as well

--- ecowitt2mqtt/data.py
+++ ecowitt2mqtt/data.py
@@ -207,7 +207,7 @@ class DataProcessor:  # pylint: disable=too-many-instance-attributes
             if self._unit_system == UNIT_SYSTEM_METRIC:
                 data[new_key] = round(inhg_to_hpa(float(self._data[original_key])), 1)
             else:
-                data[new_key] = int(float(self._data[original_key]))
+                data[new_key] = float(self._data[original_key]))
 
         return data
 
