d7c21336154a932117835a4a7ef4a272efcdef2d
Aaron Bach <bachya1208@gmail.com>
2021-08-01 17:02:28 -0600

    Simplify conversion/calculation architecture (#62)

--- ecowitt2mqtt/data.py
+++ ecowitt2mqtt/data.py
@@ -1,5 +1,7 @@
 """Define helpers to process data from an Ecowitt device."""
-from typing import Any, Dict, List, Optional, Type
+from functools import partial
+import inspect
+from typing import Any, Callable, Dict, Optional
 
 from ecowitt2mqtt.const import (
     DATA_POINT_DEWPOINT,
@@ -17,45 +19,38 @@ from ecowitt2mqtt.const import (
     UNIT_SYSTEM_IMPERIAL,
 )
 from ecowitt2mqtt.device import get_device_from_raw_payload
-from ecowitt2mqtt.helpers.converter import Converter
-from ecowitt2mqtt.helpers.converter.battery import BinaryBatteryConverter
-from ecowitt2mqtt.helpers.converter.meteo import (
-    DewPointConverter,
-    FeelsLikeConverter,
-    HeatIndexConverter,
-    PressureConverter,
-    RainConverter,
-    TemperatureConverter,
-    WindChillConverter,
-    WindSpeedConverter,
+from ecowitt2mqtt.util.battery import calculate_binary_battery
+from ecowitt2mqtt.util.meteo import (
+    calculate_dew_point,
+    calculate_feels_like,
+    calculate_heat_index,
+    calculate_pressure,
+    calculate_rain_volume,
+    calculate_temperature,
+    calculate_wind_chill,
+    calculate_wind_speed,
 )
 
 DEFAULT_KEYS_TO_IGNORE = ["PASSKEY", "dateutc", "freq", "model", "stationtype"]
 DEFAULT_UNIQUE_ID = "default"
 
+CALCULATOR_FUNCTION_MAP: Dict[str, Callable] = {
+    DATA_POINT_DEWPOINT: calculate_dew_point,
+    DATA_POINT_FEELSLIKE: calculate_feels_like,
+    DATA_POINT_GLOB_BAROM: calculate_pressure,
+    DATA_POINT_GLOB_BATT: calculate_binary_battery,
+    DATA_POINT_GLOB_RAIN: calculate_rain_volume,
+    DATA_POINT_GLOB_TEMP: calculate_temperature,
+    DATA_POINT_GLOB_WIND: calculate_wind_speed,
+    DATA_POINT_HEATINDEX: calculate_heat_index,
+    DATA_POINT_WINDCHILL: calculate_wind_chill,
+}
 
-def get_converter_class(key: str) -> Optional[Type[Converter]]:
-    """Get the proper converter class for a key."""
-    converter: Optional[Type[Converter]] = None
-    if DATA_POINT_DEWPOINT in key:
-        converter = DewPointConverter
-    if DATA_POINT_FEELSLIKE in key:
-        converter = FeelsLikeConverter
-    if DATA_POINT_GLOB_BAROM in key:
-        converter = PressureConverter
-    if DATA_POINT_GLOB_BATT in key:
-        converter = BinaryBatteryConverter
-    if DATA_POINT_GLOB_RAIN in key:
-        converter = RainConverter
-    if DATA_POINT_GLOB_TEMP in key:
-        converter = TemperatureConverter
-    if DATA_POINT_GLOB_WIND in key:
-        converter = WindSpeedConverter
-    if DATA_POINT_HEATINDEX in key:
-        converter = HeatIndexConverter
-    if DATA_POINT_WINDCHILL in key:
-        converter = WindChillConverter
-    return converter
+# NOTE: these are lists because the order of the data points matters!
+DEW_POINT_KEYS = [DATA_POINT_TEMPF, DATA_POINT_HUMIDITY]
+FEELS_LIKE_KEYS = [DATA_POINT_TEMPF, DATA_POINT_HUMIDITY, DATA_POINT_WINDSPEEDMPH]
+HEAT_INDEX_KEYS = [DATA_POINT_TEMPF, DATA_POINT_HUMIDITY]
+WIND_CHILL_KEYS = [DATA_POINT_TEMPF, DATA_POINT_WINDSPEEDMPH]
 
 
 def de_unit_key(key: str) -> str:
@@ -69,19 +64,20 @@ def de_unit_key(key: str) -> str:
     return key
 
 
+def get_data_type(key: str) -> Optional[str]:
+    """Get the data "type" (if it exists) for a specific data key."""
+    if key in CALCULATOR_FUNCTION_MAP:
+        return key
+
+    matches = [k for k in CALCULATOR_FUNCTION_MAP if k in key]
+    if matches:
+        return matches[0]
+
+    return None
+
+
 class DataProcessor:  # pylint: disable=too-few-public-methods
     """Define an object that holds processed payload data from the device."""
-
-    # Define key sets for calculated data points.
-    # NOTE: these are lists because the order of the data points matters!
-    DEW_POINT_KEYS: List[str] = [DATA_POINT_TEMPF, DATA_POINT_HUMIDITY]
-    FEELS_LIKE_KEYS: List[str] = [
-        DATA_POINT_TEMPF,
-        DATA_POINT_HUMIDITY,
-        DATA_POINT_WINDSPEEDMPH,
-    ]
-    HEAT_INDEX_KEYS: List[str] = [DATA_POINT_TEMPF, DATA_POINT_HUMIDITY]
-    WIND_CHILL_KEYS: List[str] = [DATA_POINT_TEMPF, DATA_POINT_WINDSPEEDMPH]
 
     def __init__(
         self,
@@ -91,56 +87,68 @@ class DataProcessor:  # pylint: disable=too-few-public-methods
         output_unit_system: str = UNIT_SYSTEM_IMPERIAL
     ) -> None:
         """Initialize."""
-        self._data: Dict[str, Any] = {}
-
-        for key, value in payload.items():
-            if key in DEFAULT_KEYS_TO_IGNORE:
-                continue
-            try:
-                self._data[key] = float(value)
-            except ValueError:
-                self._data[key] = value
-
+        self._calculator_funcs: Dict[str, Callable] = {}
         self._input_unit_system = input_unit_system
         self._output_unit_system = output_unit_system
+        self._payload = payload
         self.device = get_device_from_raw_payload(payload)
         self.unique_id = payload.get("PASSKEY", DEFAULT_UNIQUE_ID)
+
+    def _get_calculator_func(self, key: str) -> Optional[Callable]:
+        """Get the proper calculator function for a data point."""
+        data_type = get_data_type(key)
+
+        if not data_type:
+            return None
+
+        if data_type in self._calculator_funcs:
+            return self._calculator_funcs[data_type]
+
+        func = CALCULATOR_FUNCTION_MAP[data_type]
+
+        kwargs = {}
+        func_params = inspect.signature(func).parameters
+        if "input_unit_system" in func_params:
+            kwargs["input_unit_system"] = self._input_unit_system
+        if "output_unit_system" in func_params:
+            kwargs["output_unit_system"] = self._output_unit_system
+
+        self._calculator_funcs[data_type] = partial(func, **kwargs)
+        return self._calculator_funcs[data_type]
 
     def generate_data(self) -> Dict[str, Any]:
         """Generate a parsed data payload."""
         translated_data: Dict[str, Any] = {}
-        for key, value in self._data.items():
-            converter_class = get_converter_class(key)
+        for target_key, value in self._payload.items():
+            if target_key in DEFAULT_KEYS_TO_IGNORE:
+                continue
 
-            if converter_class:
-                converter = converter_class(  # type: ignore
-                    value,
-                    input_unit_system=self._input_unit_system,
-                    output_unit_system=self._output_unit_system,
-                )
-                target_key = de_unit_key(key)
-                translated_data[target_key] = converter.parse()
+            try:
+                value = float(value)
+            except ValueError:
+                pass
+
+            calculator = self._get_calculator_func(target_key)
+            if calculator:
+                output = calculator(value)
+                target_key = de_unit_key(target_key)
+                translated_data[target_key] = output
             else:
-                translated_data[key] = value
+                translated_data[target_key] = value
 
         calculated_data: Dict[str, Any] = {}
         for target_key, input_keys in [
-            (DATA_POINT_DEWPOINT, self.DEW_POINT_KEYS),
-            (DATA_POINT_FEELSLIKE, self.FEELS_LIKE_KEYS),
-            (DATA_POINT_HEATINDEX, self.HEAT_INDEX_KEYS),
-            (DATA_POINT_WINDCHILL, self.WIND_CHILL_KEYS),
+            (DATA_POINT_DEWPOINT, DEW_POINT_KEYS),
+            (DATA_POINT_FEELSLIKE, FEELS_LIKE_KEYS),
+            (DATA_POINT_HEATINDEX, HEAT_INDEX_KEYS),
+            (DATA_POINT_WINDCHILL, WIND_CHILL_KEYS),
         ]:
-            if not all(k in self._data for k in input_keys):
+            if not all(k in translated_data for k in input_keys):
                 continue
 
-            converter_class = get_converter_class(target_key)
-
-            if converter_class:
-                converter = converter_class(
-                    *[self._data[key] for key in input_keys],
-                    input_unit_system=self._input_unit_system,
-                    output_unit_system=self._output_unit_system
-                )
-                calculated_data[target_key] = converter.parse()
+            calculator = self._get_calculator_func(target_key)
+            if calculator:
+                output = calculator(*[self._payload[k] for k in input_keys])
+                calculated_data[target_key] = output
 
         return {**translated_data, **calculated_data}
---
+++ ecowitt2mqtt/util/meteo.py
@@ -0,0 +1,151 @@
+"""Define meteorological utilities."""
+from typing import Optional, cast
+
+import meteocalc
+
+from ecowitt2mqtt.const import LOGGER, UNIT_SYSTEM_IMPERIAL
+
+
+def _get_temperature_object(temperature: float, unit_system: str) -> meteocalc.Temp:
+    """Get a meteocalc temperature object based on a temperature and unit system."""
+    if unit_system == UNIT_SYSTEM_IMPERIAL:
+        unit = "f"
+    else:
+        unit = "c"
+    return meteocalc.Temp(temperature, unit)
+
+
+def calculate_dew_point(
+    temperature: float,
+    humidity: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate dew point in the appropriate unit system."""
+    temp_obj = _get_temperature_object(temperature, input_unit_system)
+    dew_point_obj = meteocalc.dew_point(temp_obj, humidity)
+
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        value = round(dew_point_obj.f, 1)
+    else:
+        value = round(dew_point_obj.c, 1)
+    return cast(float, value)
+
+
+def calculate_feels_like(
+    temperature: float,
+    humidity: float,
+    wind_speed: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate "feels like" temperature in the appropriate unit system."""
+    temp_obj = _get_temperature_object(temperature, input_unit_system)
+    feels_like_obj = meteocalc.feels_like(temp_obj, humidity, wind_speed)
+
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        value = round(feels_like_obj.f, 1)
+    else:
+        value = round(feels_like_obj.c, 1)
+    return cast(float, value)
+
+
+def calculate_heat_index(
+    temperature: float,
+    humidity: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate heat index in the appropriate unit system."""
+    temp_obj = _get_temperature_object(temperature, input_unit_system)
+    heat_index_obj = meteocalc.heat_index(temp_obj, humidity)
+
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        value = round(heat_index_obj.f, 1)
+    else:
+        value = round(heat_index_obj.c, 1)
+    return cast(float, value)
+
+
+def calculate_pressure(
+    value: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate pressure in the appropriate unit system."""
+    if input_unit_system == output_unit_system:
+        return value
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        return value / 33.8639
+    return value * 33.8639
+
+
+def calculate_rain_volume(
+    value: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate rain volume in the appropriate unit system."""
+    if input_unit_system == output_unit_system:
+        return value
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        return value / 25.4
+    return value * 25.4
+
+
+def calculate_temperature(
+    value: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate temperature in the appropriate unit system."""
+    temp_obj = _get_temperature_object(value, input_unit_system)
+
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        value = round(temp_obj.f, 1)
+    else:
+        value = round(temp_obj.c, 1)
+    return value
+
+
+def calculate_wind_chill(
+    temperature: float,
+    wind_speed: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> Optional[float]:
+    """Calculate wind chill in the appropriate unit system."""
+    temp_obj = _get_temperature_object(temperature, input_unit_system)
+
+    try:
+        wind_chill_obj = meteocalc.wind_chill(temp_obj, wind_speed)
+    except ValueError as err:
+        LOGGER.debug("%s (temperature: %s, wind speed: %s)", err, temp_obj, wind_speed)
+        return None
+
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        value = round(wind_chill_obj.f, 1)
+    else:
+        value = round(wind_chill_obj.c, 1)
+    return cast(float, value)
+
+
+def calculate_wind_speed(
+    value: float,
+    *,
+    input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
+    output_unit_system: str = UNIT_SYSTEM_IMPERIAL
+) -> float:
+    """Calculate wind speed in the appropriate unit system."""
+    if input_unit_system == output_unit_system:
+        return value
+    if output_unit_system == UNIT_SYSTEM_IMPERIAL:
+        return value / 1.60934
+    return value * 1.60934
