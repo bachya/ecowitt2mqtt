9b259be74956873c8726eb13796cbf447028920b
Aaron Bach <bachya1208@gmail.com>
2021-08-19 16:02:48 -0600

    Handle non-binary battery values

--- ecowitt2mqtt/util/battery.py
+++ ecowitt2mqtt/util/battery.py
@@ -1,10 +1,21 @@
 """Define battery utilities."""
+from typing import Union
+
 BATTERY_STATE_OFF = "off"
 BATTERY_STATE_ON = "on"
 
 
-def calculate_binary_battery(value: float) -> str:
-    """Calculate a "binary battery" value (OK/Low)."""
+def calculate_battery(value: Union[float, int]) -> Union[float, str]:
+    """Calculate a battery value.
+
+    1. If the value is a float, we assume it represents voltage and return it as-is.
+    2. If the value is an int, we assume it represents a binary state:
+         * 0: OK
+         * 1: Low
+    """
+    if isinstance(value, float):
+        return value
+
     if value == 0:
         return BATTERY_STATE_OFF
     return BATTERY_STATE_ON
