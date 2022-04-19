2565b99489ce6e554e5116017ebef881710580c7
Aaron Bach <bachya1208@gmail.com>
2022-04-19 11:36:15 -0600

    Fix bug where lightning strike distance is a string (#119)

--- ecowitt2mqtt/util/distance.py
+++ ecowitt2mqtt/util/distance.py
@@ -1,16 +1,20 @@
 """Define distance utilities."""
+from typing import Any
+
 from ecowitt2mqtt.const import UNIT_SYSTEM_IMPERIAL
 
 
 def calculate_distance(
-    value: float,
+    value: Any,
     *,
     input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
     output_unit_system: str = UNIT_SYSTEM_IMPERIAL
 ) -> float:
     """Calculate distance in the appropriate unit system."""
+    float_value = float(value)
+
     if input_unit_system == output_unit_system:
-        return value
+        return float_value
     if output_unit_system == UNIT_SYSTEM_IMPERIAL:
-        return round(value / 1.609, 1)
-    return round(value * 1.609, 1)
+        return round(float_value / 1.609, 1)
+    return round(float_value * 1.609, 1)
