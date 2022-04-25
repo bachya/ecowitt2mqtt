74513967c8914968a836b39d2ff7b51649537a5c
Aaron Bach <bachya1208@gmail.com>
2022-04-24 21:12:19 -0600

    Handle case where distance conversion function is handled bad input (#122)

--- ecowitt2mqtt/util/distance.py
+++ ecowitt2mqtt/util/distance.py
@@ -1,7 +1,7 @@
 """Define distance utilities."""
-from typing import Any
+from typing import Any, Optional
 
-from ecowitt2mqtt.const import UNIT_SYSTEM_IMPERIAL
+from ecowitt2mqtt.const import LOGGER, UNIT_SYSTEM_IMPERIAL
 
 
 def calculate_distance(
@@ -9,9 +9,13 @@ def calculate_distance(
     *,
     input_unit_system: str = UNIT_SYSTEM_IMPERIAL,
     output_unit_system: str = UNIT_SYSTEM_IMPERIAL
-) -> float:
+) -> Optional[float]:
     """Calculate distance in the appropriate unit system."""
-    float_value = float(value)
+    try:
+        float_value = float(value)
+    except ValueError:
+        LOGGER.debug("Could not convert distance value to float: %s", value)
+        return None
 
     if input_unit_system == output_unit_system:
         return float_value
