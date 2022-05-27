babfdcebb1f479e699de3b3afd06945b772203af
Aaron Bach <bachya1208@gmail.com>
2022-05-27 16:06:34 -0600

    Give raw config fixture a more appropriate name

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,19 @@ from typer.testing import CliRunner
 from .common import TEST_RAW_JSON
 
 
-@pytest.fixture(name="config", scope="session")
-def config_fixture():
-    """Define a fixture to return raw configuration data."""
-    return TEST_RAW_JSON
-
-
 @pytest.fixture(name="config_filepath")
-def config_filepath_fixture(config, tmp_path):
+def config_filepath_fixture(raw_config, tmp_path):
     """Define a fixture to return a config filepath."""
     config_filepath = f"{tmp_path}/config.json"
     with open(config_filepath, "w", encoding="utf-8") as config_file:
-        config_file.write(config)
+        config_file.write(raw_config)
     return config_filepath
+
+
+@pytest.fixture(name="raw_config", scope="session")
+def raw_config_fixture():
+    """Define a fixture to return raw configuration data."""
+    return TEST_RAW_JSON
 
 
 @pytest.fixture(name="runner")
