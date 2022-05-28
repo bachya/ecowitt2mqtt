554dc07e19fb98b4816ef56d436187d0a3d9d6d3
Aaron Bach <bachya1208@gmail.com>
2022-05-27 20:31:51 -0600

    Abstract logging setup

--- ecowitt2mqtt/core.py
+++ ecowitt2mqtt/core.py
@@ -14,24 +14,27 @@ from ecowitt2mqtt.helpers.logging import TyperLoggerHandler
 from ecowitt2mqtt.server import Server
 
 
+def setup_logging(verbose: bool = False) -> None:
+    """Set up logging."""
+    if verbose or os.getenv(LEGACY_ENV_LOG_LEVEL):
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        handlers=(TyperLoggerHandler(),),
+    )
+
+
 class Ecowitt:  # pylint: disable=too-few-public-methods
     """Define the base application object."""
 
     def __init__(self, params: dict[str, Any]) -> None:
         """Initialize."""
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
-        if params.get(CONF_VERBOSE, False) or os.getenv(LEGACY_ENV_LOG_LEVEL):
-            log_level = logging.DEBUG
-        else:
-            log_level = logging.INFO
-
-        typer_handler = TyperLoggerHandler()
-        logging.basicConfig(
-            level=log_level,
-            format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
-            handlers=(typer_handler,),
-        )
+        setup_logging(params.get(CONF_VERBOSE, False))
 
         self.config = Config(params)
         self.server = Server(self)
