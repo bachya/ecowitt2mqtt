b9a1c3540632b4fcb20db2a6263497ed7342b8c3
Aaron Bach <bachya1208@gmail.com>
2022-07-31 14:51:46 -0600

    Bump version to 2022.07.7

--- ecowitt2mqtt/const.py
+++ ecowitt2mqtt/const.py
@@ -4,7 +4,7 @@ from typing import Final
 
 from ecowitt2mqtt.helpers.typing import UnitSystemType
 
-__version__ = "2022.07.6"
+__version__ = "2022.07.7"
 
 
 LOGGER = logging.getLogger(__package__)
