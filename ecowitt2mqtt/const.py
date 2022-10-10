a57694767792f2354365ba10b45f9ac9b4119f41
Aaron Bach <bachya1208@gmail.com>
2022-10-10 13:36:49 -0600

    Add SpeedConverter helper (#309)
    * Add SpeedConverter helper
    
    * Update
    
    * Better test
    
    * Better test
    
    * Leak
    
    * More compact unit validity check

--- ecowitt2mqtt/const.py
+++ ecowitt2mqtt/const.py
@@ -199,8 +199,14 @@ PRESSURE_HPA: Final = "hPa"
 PRESSURE_INHG: Final = "inHg"
 
 # Speed units:
+SPEED_FEET_PER_SECOND: Final = "ft/s"
+SPEED_INCHES_PER_DAY: Final = "in/d"
+SPEED_INCHES_PER_HOUR: Final = "in/h"
 SPEED_KILOMETERS_PER_HOUR: Final = "km/h"
+SPEED_KNOTS: Final = "kn"
+SPEED_METERS_PER_SECOND: Final = "m/s"
 SPEED_MILES_PER_HOUR: Final = "mph"
+SPEED_MILLIMETERS_PER_DAY: Final = "mm/d"
 
 # Temperature units:
 TEMP_CELSIUS: Final = "°C"
---
+++ tests/util/test_unit_conversion.py
@@ -0,0 +1,38 @@
+"""Test unit conversion helpers."""
+import pytest
+
+from ecowitt2mqtt.util.unit_conversion import SpeedConverter, UnitConversionError
+
+
+@pytest.mark.parametrize(
+    "unit_class,converter,from_unit,to_unit",
+    [
+        ("speed", SpeedConverter, "mph", "km/s"),
+        ("speed", SpeedConverter, "km/d", "m/s"),
+    ],
+)
+def test_invalid_units(converter, from_unit, to_unit, unit_class):
+    """Test that invalid units raise an error."""
+    with pytest.raises(UnitConversionError) as err:
+        _ = converter.convert(10, from_unit, to_unit)
+        assert f"is not a recognized {unit_class} unit" in str(err)
+
+
+@pytest.mark.parametrize(
+    "value,from_unit,to_unit,converted_value",
+    [
+        (10, "ft/s", "m/s", 3.0479999999999996),
+        (1000, "in/d", "m/s", 0.00029398148148148144),
+        (100, "in/h", "m/s", 0.0007055555555555556),
+        (100, "km/h", "m/s", 27.77777777777778),
+        (10, "kn", "m/s", 5.144444444444445),
+        (1, "m/s", "m/s", 1.0),
+        (100, "mph", "m/s", 44.70399999999999),
+        (10000, "mm/d", "m/s", 0.00011574074074074075),
+        (1, "m/s", "in/d", 3401574.8031496066),
+        (10, "in/h", "km/h", 0.000254),
+    ],
+)
+def test_speed_conversion(converted_value, from_unit, to_unit, value):
+    """Test speed conversions."""
+    assert SpeedConverter.convert(value, from_unit, to_unit) == converted_value
