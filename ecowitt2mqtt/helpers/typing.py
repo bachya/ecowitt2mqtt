4d1601de4b72bf0e723f0620591b6045f289bae1
Aaron Bach <bachya1208@gmail.com>
2022-10-30 14:29:53 -0600

    Fix typing

--- ecowitt2mqtt/helpers/typing.py
+++ ecowitt2mqtt/helpers/typing.py
@@ -1,8 +1,7 @@
 """Define typing helpers."""
-from collections.abc import Collection
 from datetime import datetime
 from typing import Literal
 
-CalculatedValueType = Collection[str] | float | str | datetime | None
+CalculatedValueType = list[str] | float | str | datetime | None
 PreCalculatedValueType = float | str
 UnitSystemType = Literal["imperial", "metric"]
