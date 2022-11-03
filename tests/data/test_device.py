26699eae3750ded9e5b2d9b860a96fca2901cfb5
Aaron Bach <bachya1208@gmail.com>
2022-11-03 11:18:09 -0600

    Add more tests to validate device mappings (#372)

--- tests/data/test_device.py
+++ tests/data/test_device.py
@@ -18,9 +18,45 @@ from ecowitt2mqtt.helpers.device import Device
                 "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
                 "Ecowitt",
                 "GW1000",
+                "GW1000B_V1.6.8",
+            ),
+            "payload_gw1000pro.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Ecowitt",
+                "GW1000",
                 "GW1000B_V1.7.3",
             ),
             "payload_gw1000bpro.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Ecowitt",
+                "GW1100",
+                "GW1100B_V2.0.3",
+            ),
+            "payload_gw1100b.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Ecowitt",
+                "GW2000A",
+                "GW2000A_V2.1.4",
+            ),
+            "payload_gw2000a_1.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Misol",
+                "HP2250_Pro",
+                "EasyWeatherV1.5.9",
+            ),
+            "payload_pthp2550pro.json",
         ),
         (
             Device(
@@ -39,6 +75,24 @@ from ecowitt2mqtt.helpers.device import Device
                 "Unknown Station Type",
             ),
             "payload_unknown_2.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Fine Offset",
+                "WH2650",
+                "WH2650A_V1.7.4",
+            ),
+            "payload_wh2650a.json",
+        ),
+        (
+            Device(
+                "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                "Ambient Weather",
+                "WS-2902C",
+                "EasyWeatherV1.5.9",
+            ),
+            "payload_ws2900.json",
         ),
     ],
 )
