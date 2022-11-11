38c51793ad3caad1232e89707252fe43a277267e
Aaron Bach <bachya1208@gmail.com>
2022-11-11 07:36:41 -0700

    Fix type names to avoid a pylint override

--- ecowitt2mqtt/helpers/calculator/__init__.py
+++ ecowitt2mqtt/helpers/calculator/__init__.py
@@ -15,11 +15,9 @@ from ecowitt2mqtt.util.unit_conversion import BaseUnitConverter
 if TYPE_CHECKING:
     from ecowitt2mqtt.config import Config
 
-_CalculatorType = TypeVar(  # pylint: disable=invalid-name
-    "_CalculatorType", bound="Calculator"
-)
-_CalculateFromPayloadFuncType = Callable[
-    [_CalculatorType, dict[str, PreCalculatedValueType]], "CalculatedDataPoint"
+_CalculatorT = TypeVar("_CalculatorType", bound="Calculator")
+_CalculateFromPayloadFuncT = Callable[
+    [_CalculatorT, dict[str, PreCalculatedValueType]], "CalculatedDataPoint"
 ]
 
 
@@ -166,7 +164,7 @@ class Calculator:
     @staticmethod
     def requires_keys(
         *keys: Iterable[str],
-    ) -> Callable[[_CalculateFromPayloadFuncType], _CalculateFromPayloadFuncType]:
+    ) -> Callable[[_CalculateFromPayloadFuncT], _CalculateFromPayloadFuncT]:
         """Define a decorator that requires certain payload keys to exist.
 
         Args:
@@ -190,7 +188,7 @@ class Calculator:
 
             @wraps(func)
             def wrapper(
-                calculator: _CalculatorType, payload: dict[str, PreCalculatedValueType]
+                calculator: _CalculatorT, payload: dict[str, PreCalculatedValueT]
             ) -> CalculatedDataPoint:
                 """Wrap.
 
