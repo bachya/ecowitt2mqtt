2080d0628fbf7222e488c39b88094a5d4a35cbee
Aaron Bach <bachya1208@gmail.com>
2022-11-18 12:33:43 -0700

    Bump version to 2022.11.0

--- ecowitt2mqtt/const.py
+++ ecowitt2mqtt/const.py
@@ -4,7 +4,7 @@ from typing import Final
 
 from ecowitt2mqtt.helpers.typing import UnitSystemType
 
-__version__ = "2022.10.4"
+__version__ = "2022.11.0"
 
 LOGGER = logging.getLogger(__package__)
 
