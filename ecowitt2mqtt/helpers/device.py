ab265812bd0be6f111ed94bfce5d263e2e7d38ec
Aaron Bach <bachya1208@gmail.com>
2022-11-26 16:59:52 -0700

    Expand device parsing to cover more use cases (#399)

--- ecowitt2mqtt/helpers/device.py
+++ ecowitt2mqtt/helpers/device.py
@@ -4,33 +4,25 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 
-from ecowitt2mqtt.const import LOGGER
+from ecowitt2mqtt.util import glob_search
 
-DEFAULT_MANUFACTURER = "Unknown"
+DEFAULT_MANUFACTURER = "Unknown Manufacturer"
+DEFAULT_MODEL = "Unknown Model"
 DEFAULT_NAME = "Unknown Device"
-DEFAULT_STATION_TYPE = "Unknown Station Type"
-DEFAULT_UNIQUE_ID = "default"
 
+MODEL_BRAND_MAP = {
+    "GW1000": "Ecowitt",
+    "GW1100": "Ecowitt",
+    "GW2000": "Ecowitt",
+    "HP2550": "Misol",
+    "PT-HP2550": "Fine Offset",
+    "WH2650": "Fine Offset",
+    "WS2350": "La Crosse",
+    "WS2900": "Ambient Weather",
+}
 
-@dataclass
-class MappedDevice:
-    """Define a registered device."""
-
-    brand: str
-    model: str
-
-
-DEVICE_MAP = {
-    "GW1000": MappedDevice("Ecowitt", "GW1000"),
-    "GW1100": MappedDevice("Ecowitt", "GW1100"),
-    "GW2000A": MappedDevice("Ecowitt", "GW2000A"),
-    "GW2000B": MappedDevice("Ecowitt", "GW2000B"),
-    "GW2000C": MappedDevice("Ecowitt", "GW2000C"),
-    "HP2550_Pro": MappedDevice("Misol", "HP2250_Pro"),
-    "PT-HP2550": MappedDevice("Fine Offset", "HP2550"),
-    "WH2650": MappedDevice("Fine Offset", "WH2650"),
-    "WS2350": MappedDevice("La Crosse", "WS-2350"),
-    "WS2900": MappedDevice("Ambient Weather", "WS-2902C"),
+STATION_TYPE_BRAND_MAP = {
+    "AMBWeather": "Ambient Weather",
 }
 
 
@@ -38,10 +30,11 @@ DEVICE_MAP = {
 class Device:
     """Define a data object to provide device details."""
 
-    unique_id: str
     manufacturer: str
+    model: str
     name: str
     station_type: str
+    unique_id: str
 
 
 def get_device_from_raw_payload(payload: dict[str, Any]) -> Device:
@@ -53,23 +46,24 @@ def get_device_from_raw_payload(payload: dict[str, Any]) -> Device:
     Returns:
         A parsed Device object.
     """
-    model = payload["model"]
-    station_type = payload.get("stationtype", DEFAULT_STATION_TYPE)
-    unique_id = payload.get("PASSKEY", DEFAULT_UNIQUE_ID)
+    station_type = payload["stationtype"]
+    unique_id = payload["PASSKEY"]
 
-    if model in DEVICE_MAP:
-        mapped_device = DEVICE_MAP[model]
+    if model := payload.get("model"):
+        name, manufacturer = glob_search(MODEL_BRAND_MAP, model)
     else:
-        if matches := [v for k, v in DEVICE_MAP.items() if k in model]:
-            mapped_device = matches[0]
-        else:
-            LOGGER.info(
-                (
-                    "Unknown device; please report it at "
-                    "https://github.com/bachya/ecowitt2mqtt (payload: %s)"
-                ),
-                payload,
-            )
-            mapped_device = MappedDevice(DEFAULT_MANUFACTURER, DEFAULT_NAME)
+        model = DEFAULT_MODEL
+        name, manufacturer = glob_search(STATION_TYPE_BRAND_MAP, station_type)
 
-    return Device(unique_id, mapped_device.brand, mapped_device.model, station_type)
+    if not manufacturer:
+        manufacturer = DEFAULT_MANUFACTURER
+    if not name:
+        name = DEFAULT_NAME
+
+    return Device(
+        manufacturer=manufacturer,
+        model=model,
+        name=name,
+        station_type=station_type,
+        unique_id=unique_id,
+    )
