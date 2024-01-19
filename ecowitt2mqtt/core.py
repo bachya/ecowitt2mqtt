67014b7691f353accf4fe5f1647b252a9ee7cfdc
Aaron Bach <bachya1208@gmail.com>
2024-01-19 09:06:04 -0700

    more

--- ecowitt2mqtt/core.py
+++ ecowitt2mqtt/core.py
@@ -9,7 +9,7 @@ from typing import Any
 import colorlog
 
 from ecowitt2mqtt.config import ConfigError, Configs
-from ecowitt2mqtt.const import LEGACY_ENV_LOG_LEVEL, LOGGER, __version__
+from ecowitt2mqtt.const import LOGGER, __version__
 from ecowitt2mqtt.runtime import Runtime
 
 
@@ -19,7 +19,7 @@ def configure_logging(verbose: bool) -> None:
     Args:
         verbose: Whether verbose logging should be included.
     """
-    if verbose or os.getenv(LEGACY_ENV_LOG_LEVEL):
+    if verbose:
         log_level = logging.DEBUG
     else:
         log_level = logging.INFO
