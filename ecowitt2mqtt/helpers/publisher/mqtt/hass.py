796dac957daf2d4df95ce8dab1301a956ed250cd
Ignacio Hernandez-Ros <ignacio@hernandez-ros.com>
2025-11-26 12:45:30 +0100

    Add default_entity_id to MQTT discovery configuration
    rolled back previous change to keep object_id
    added default_entity_id using platform.object_id

--- ecowitt2mqtt/helpers/publisher/mqtt/hass.py
+++ ecowitt2mqtt/helpers/publisher/mqtt/hass.py
@@ -1,4 +1,4 @@
-"""Define MQTT publishing."""
+ƒ"""Define MQTT publishing."""
 
 # pylint: disable=unused-argument
 from __future__ import annotations
@@ -181,6 +181,7 @@ class HassDiscoveryInfo:
     device_class: str | None = None
     entity_category: str | None = None
     icon: str | None = None
+    object_id: str | None = None
     default_entity_id: str | None = None
     qos: int = 1
     state_class: str | None = None
@@ -518,7 +519,8 @@ class HomeAssistantDiscoveryPublisher(MqttPublisher):  # pylint: disable=too-few
         )
 
         if self._config.hass_entity_id_prefix:
-            discovery.default_entity_id = f"{self._config.hass_entity_id_prefix}_{payload_key}"
+            discovery.object_id = f"{self._config.hass_entity_id_prefix}_{payload_key}"
+            discovery.default_entity_id = f"{PLATFORM_MAP[data_point.data_type]}.{discovery.object_id}"
         if data_point.unit:
             discovery.unit_of_measurement = data_point.unit
 
